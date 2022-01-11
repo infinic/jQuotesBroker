05bc78bf4d0a352165e8f2ae84407aec66fd19b4
infinic <admlynx@gmail.com>
2022-01-11 14:55:08 +0500

    Init

---
+++ src/main/java/ru/infinic/jquotesbroker/JQuotesBrokerApplication.java
@@ -0,0 +1,13 @@
+package ru.infinic.jquotesbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JQuotesBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JQuotesBrokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/infinic/jquotesbroker/JQuotesBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.infinic.jquotesbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JQuotesBrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
